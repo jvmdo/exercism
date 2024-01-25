318fce59f04107f53c8f5460e9cb0f49a4dab254
jvmdo <deoliveira.jvm@gmail.com>
2024-01-25 15:11:44 -0300

    add Lasagna

---
+++ php/lasagna/Lasagna.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+class Lasagna
+{
+    public function expectedCookTime(): int
+    {
+        return 40;
+    }
+
+    public function remainingCookTime(int $elapsed_minutes): int
+    {
+        return $this->expectedCookTime() - $elapsed_minutes;
+    }
+
+    public function totalPreparationTime(int $layers_to_prep): int
+    {
+        return $layers_to_prep * 2;
+    }
+
+    function totalElapsedTime(int $layers_to_prep, int $elapsed_minutes): int
+    {
+        return $this->totalPreparationTime($layers_to_prep) + $elapsed_minutes;
+    }
+
+    function alarm(): string
+    {
+        return "Ding!";
+    }
+}
